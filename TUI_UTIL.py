99b4da83f074932fe8549ce1398d9c2f4a378673
Buzatel-Iulian <iulian.buzatel@gmail.com>
2023-04-05 23:32:53 +0300

    Fixed some stuff

--- TUI_UTIL.py
+++ TUI_UTIL.py
@@ -17,13 +17,13 @@ class Status:
     def show(self):
         y, x = self.elem.getmaxyx()
         ad_hline(self.elem, 0, 0, curses.ACS_HLINE, x)
-        ad_str(self.elem, 0, 0, self.stat, curses.A_STANDOUT)
+        ad_str(self.elem, 0, 0, self.stat+" ")
 
 class Tui:
     def __init__(self):
         self.screen = curses.initscr()
         self.screen.clear()
-        self.STATUS = Status(self.screen, "Use arrow keys to go up and down, Press enter to select a choice")
+        self.STATUS = Status(self.screen, "Use arrow keys to go up and down, Press enter to interact")
         self.win = []
         self.sel = 0
         self.prev_sel = 0
@@ -64,6 +64,7 @@ class Tui:
                 self.win[self.sel].key_down()
             elif char == ord("\n"):  # Enter
                 self.win[self.sel].function()
+                self.STATUS.show()
             elif char == 27 :
                 self.screen.nodelay(True)
                 n = self.screen.getch()
@@ -112,7 +113,7 @@ class Menu:
         self.widgets = []
         self.key = key
         self.arrow = 0
-    def addButton(self, button):
+    def add(self, button):
         button.elem = self.win
         self.widgets.append(button)
     def show(self, current):
@@ -136,10 +137,17 @@ class Menu:
         if self.arrow < 0:
             self.arrow = aux
     def key_up(self):
-        self.arrow +=1
+        self.arrow -=1
+        while not(self.widgets[self.arrow].active):
+            self.arrow -=1
+            self.fit()
         self.fit()
     def key_down(self):
-        self.arrow -=1
+        self.arrow +=1
+        self.fit()
+        while not(self.widgets[self.arrow].active):
+            self.arrow +=1
+            self.fit()
         self.fit()
     def reset(self, stdscr_):
         if self.scaling:
@@ -153,6 +161,7 @@ class Menu:
 
 class Button:
     def __init__(self, text, func):
+        self.active = True
         self.elem = None
         self.text = text
         self.func = func
@@ -160,7 +169,13 @@ class Button:
     def function(self):
         self.func()
     def show(self, x, sel):
-        ad_str(self.elem, x, 2, cr(self.text, self.crop), curses.A_UNDERLINE if sel else curses.A_DIM)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        if sel:
+            self.elem.attron(curses.color_pair(2))
+            ad_str(self.elem, x, 2, cr(self.text, self.crop))
+            self.elem.attroff(curses.color_pair(2))
+            return
+        ad_str(self.elem, x, 2, cr(self.text, self.crop))
         #self.elem.refresh()
     def reset(self, stdscr_):
         aux, self.crop = self.elem.getmaxyx()
@@ -168,17 +183,24 @@ class Button:
 
 class Toggle:
     def __init__(self, text, func = None):
+        self.active = True
         self.elem = None
         self.text = text
         self.func = func
-        self.toggle = toggle
         self.onoff = False
         self.crop = 3
     def function(self):
-        self.func()
+        if self.func != None:
+            self.func()
         self.onoff = not(self.onoff)
     def show(self, x, sel):
-        ad_str(self.elem, x, 1, ("+" if self.onoff else "-") + cr(self.text, self.crop), curses.A_UNDERLINE if sel else curses.A_DIM)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        if sel:
+            self.elem.attron(curses.color_pair(2))
+            ad_str(self.elem, x, 1, ("+" if self.onoff else "-") + cr(self.text, self.crop))
+            self.elem.attroff(curses.color_pair(2))
+            return
+        ad_str(self.elem, x, 1, ("+" if self.onoff else "-") + cr(self.text, self.crop))
         #self.elem.refresh()
     def reset(self, stdscr_):
         aux, self.crop = self.elem.getmaxyx()
@@ -186,12 +208,13 @@ class Toggle:
 
 class Label:
     def __init__(self, text = ""):
+        self.active = False
         self.elem = None
         self.stdscr = None
         self.text = text
         self.crop = 3
     def function(self):
-            display(self.text)
+        return
     def reset(self, stdscr_):
         self.stdscr = stdscr_
         aux, self.crop = self.elem.getmaxyx()
@@ -201,17 +224,24 @@ class Label:
 
 class Text:
     def __init__(self, text = ""):
+        self.active = True
         self.elem = None
         self.stdscr = None
         self.text = text
         self.crop = 3
     def function(self):
-        self.text = read(self.stdscr)
+        self.text = read(self.stdscr, "Text Field", self.text)
     def reset(self, stdscr_):
         self.stdscr = stdscr_
         aux, self.crop = self.elem.getmaxyx()
         self.crop -= 3
     def show(self, x, sel):
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
+        if sel:
+            self.elem.attron(curses.color_pair(3))
+            ad_str(self.elem, x, 2, cr(self.text, self.crop))
+            self.elem.attroff(curses.color_pair(3))
+            return
         ad_str(self.elem, x, 2, cr(self.text, self.crop), curses.A_REVERSE)
 
 def sc(v_in, v_dim):
@@ -222,81 +252,12 @@ def cr(str_in, cr_dim):
         return str_in[0:(cr_dim-2)]+".."
     return str_in
 
-"""def print_menu(menu_win, h_, menu_h, _cursor = "   "): #, highlight_y):
-    
-    x = 2
-    y = 2
-    i = 0
-    menu_win["win"].box(0, 0)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    #curses.init_pair(3, curses.A_UNDERLINE, curses.COLOR_BLACK)
-
-    for widget in menu_win["widgets"]:
-        if menu_h and h_ == i + 1:
-            menu_win["win"].attron(curses.color_pair(2))
-            if menu_win["widgets"][i]["type"] == "label":
-                ad_str(menu_win["win"], y + i, x, _cursor + menu_win["widgets"][i]["text"] + "  ", curses.A_UNDERLINE)
-            else:
-                ad_str(menu_win["win"], y + i, x, _cursor + menu_win["widgets"][i]["text"] + " ")
-            menu_win["win"].attroff(curses.color_pair(2))
-        else:
-            if menu_win["widgets"][i]["type"] == "label":
-                #menu_win["win"].attron(curses.color_pair(3))
-                ad_str(menu_win["win"], y + i, x, "   " + menu_win["widgets"][i]["text"]+"   ", curses.A_UNDERLINE)
-                #menu_win["win"].attroff(curses.color_pair(3))
-            else:
-                ad_str(menu_win["win"], y + i, x, "- " + menu_win["widgets"][i]["text"] + " ")
-        i = i + 1
-
-    menu_win["win"].refresh()
-"""
-
-def display(stdscr_, text = "nothing"):
-    stdscr_.timeout(100)
-    y, x = stdscr_.getyx()
-    text = txt
-    orig = txt
-    win = curses.newwin(5, 44, 1, 2)
-    win.box(0, 0)
-    ad_str(win, 1, 1, t_name, curses.A_UNDERLINE)
-    ad_str(win, 2, 1, text, curses.A_DIM)
-    while True:
-        char = elem.getch()
-        if char == ord("\n"):
-            break
-        elif char == curses.KEY_BACKSPACE:
-            text = text[0:-1]
-            win.clear()
-            win.box(0, 0)
-            ad_str(win, 1, 1, t_name, curses.A_UNDERLINE)
-            #win.refresh()
-        elif char == 27 :
-            #elem.nodelay(True)
-            n = elem.getch()
-            if n == -1:
-            # Escape was pressed
-                elem.timeout(REFRESH_RATE)
-                elem.clear()
-                return orig
-            #elem.nodelay(False)
-        else:
-            try:
-                text += chr(char)
-            except:
-                pass
-        ad_str(win, 2, 1, text + "_")
-        #elem.refresh()
-        win.refresh()
-    elem.timeout(REFRESH_RATE)
-    elem.clear()
-    return text
-
 def read(elem, t_name = "Text Input     ", txt = ""):
     elem.timeout(100)
-    y, x = elem.getyx()
+    max_y, max_x = elem.getmaxyx()
     text = txt
     orig = txt
-    win = curses.newwin(5, 44, 1, 2)
+    win = curses.newwin(sc(30, max_y), sc(80, max_x), sc(35, max_y), sc(10, max_x))
     win.box(0, 0)
     ad_str(win, 1, 1, t_name, curses.A_UNDERLINE)
     ad_str(win, 2, 1, text, curses.A_DIM)
